1389caf33ed410b9aa2e23a1dffa3e1d193c9dfc
mohammedshafeeq95 <120107758+mohammedshafeeq95@users.noreply.github.com>
2023-09-20 14:10:19 +0300

    nav bar

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,15 +1,20 @@
 import 'package:bicrypto/Controllers/Auth/login_controller.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:bicrypto/Style/styles.dart'; // <-- Import your custom theme
 
 class HomeView extends StatelessWidget {
-  final LoginController loginController = Get.find(); // <-- Use LoginController
+  final LoginController loginController = Get.find();
+
+  HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
+        backgroundColor: appTheme.primaryColor,
       ),
       body: Center(
         child: Column(
@@ -20,20 +25,42 @@ class HomeView extends StatelessWidget {
                 loginController.isLoggedIn.value
                     ? 'Welcome, ${loginController.userEmail.value}'
                     : 'Not logged in',
-                style: TextStyle(fontSize: 20),
+                style: appTheme.textTheme.bodyLarge,
               ),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Perform logout operation here
                 loginController.logout();
-                Get.offAllNamed('/login'); // Navigate to login screen
+                Get.offAllNamed('/login');
               },
               child: Text('Logout'),
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        height: 50.0, // New height
+        animationDuration: Duration(milliseconds: 300), // New duration
+        animationCurve: Curves.easeInOut, // New curve
+        index: 2, // New initial index
+        backgroundColor: appTheme.scaffoldBackgroundColor,
+        items: <Widget>[
+          Icon(Icons.home, size: 30, color: appTheme.secondaryHeaderColor),
+          Icon(Icons.pie_chart, size: 30, color: appTheme.secondaryHeaderColor),
+          Icon(Icons.swap_horiz,
+              size: 30, color: appTheme.secondaryHeaderColor),
+          Icon(Icons.show_chart,
+              size: 30, color: appTheme.secondaryHeaderColor),
+          Icon(Icons.account_balance_wallet,
+              size: 30, color: appTheme.secondaryHeaderColor),
+        ],
+        onTap: (index) {
+          // Handle your button tap here
+        },
+        color: appTheme.hintColor,
+        buttonBackgroundColor: appTheme.hintColor,
+        letIndexChange: (index) => true, // New touch area
       ),
     );
   }
