4fe6759dee493ae3734b5e76d4dedab23905fab0
mohammedshafeeq95 <120107758+mohammedshafeeq95@users.noreply.github.com>
2023-10-20 14:13:13 +0300

    ee

--- lib/views/market/markethome.dart
+++ lib/views/market/markethome.dart
@@ -3,7 +3,7 @@ import 'package:bicrypto/Style/styles.dart';
 import 'package:bicrypto/services/market_service.dart';
 import 'package:bicrypto/widgets/market/pairs_list.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart'; // import cupertino package
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class MarketScreen extends StatelessWidget {
@@ -13,73 +13,52 @@ class MarketScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8),
-          child: CupertinoSearchTextField(
-            placeholder: 'Search coin pairs',
-            style: TextStyle(color: Colors.white),
-            placeholderStyle: TextStyle(color: Colors.grey[400]),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: CupertinoSearchTextField(
+              placeholder: 'Search coin pairs',
+              style: TextStyle(color: Colors.white),
+              placeholderStyle: TextStyle(color: Colors.grey[400]),
+            ),
           ),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Top Gainers'),
+              Tab(text: 'Top Losers'),
+            ],
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.grey[400],
+            indicator: BoxDecoration(
+                border: Border(
+                    bottom: BorderSide(color: Colors.orange, width: 1.0))),
+          ),
+          backgroundColor: appTheme.scaffoldBackgroundColor,
+          elevation: 0,
         ),
-        backgroundColor: appTheme.scaffoldBackgroundColor,
-        elevation: 0,
-      ),
-      body: Column(
-        children: [
-          Obx(() => Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      _marketController.showGainers.value = true;
-                    },
-                    child: Text(
-                      'Top Gainers',
-                      style: TextStyle(
-                        fontSize: _marketController.showGainers.value ? 20 : 16,
-                        color: _marketController.showGainers.value
-                            ? Colors.white
-                            : Colors.grey[400],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 20),
-                  GestureDetector(
-                    onTap: () {
-                      _marketController.showGainers.value = false;
-                    },
-                    child: Text(
-                      'Top Losers',
-                      style: TextStyle(
-                        fontSize:
-                            !_marketController.showGainers.value ? 20 : 16,
-                        color: !_marketController.showGainers.value
-                            ? Colors.white
-                            : Colors.grey[400],
-                      ),
-                    ),
-                  ),
-                ],
-              )),
-          SizedBox(height: 10),
-          Expanded(
-            child: Obx(() {
-              List<Market> sortedMarkets =
-                  List.from(_marketController.markets.value);
-              if (_marketController.showGainers.value) {
-                sortedMarkets.sort((a, b) => b.change.compareTo(a.change));
-              } else {
-                sortedMarkets.sort((a, b) => a.change.compareTo(b.change));
-              }
-              return _marketController.isLoading.value
-                  ? const Center(child: CircularProgressIndicator())
-                  : pairs_listview(markets: sortedMarkets);
-            }),
-          ),
-        ],
+        body: TabBarView(
+          children: [
+            Obx(() => _buildMarketList(showGainers: true)),
+            Obx(() => _buildMarketList(showGainers: false)),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget _buildMarketList({required bool showGainers}) {
+    List<Market> sortedMarkets = List.from(_marketController.markets.value);
+    if (showGainers) {
+      sortedMarkets.sort((a, b) => b.change.compareTo(a.change));
+    } else {
+      sortedMarkets.sort((a, b) => a.change.compareTo(b.change));
+    }
+
+    return _marketController.isLoading.value
+        ? const Center(child: CircularProgressIndicator())
+        : pairs_listview(markets: sortedMarkets);
   }
 }
