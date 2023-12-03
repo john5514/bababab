4d7f2ed1638b11c0d482a1f8bfde076b53dbad93
mohammedshafeeq95 <120107758+mohammedshafeeq95@users.noreply.github.com>
2023-12-03 19:31:49 +0300

    Update fiat.dart

--- lib/views/webview/fiat.dart
+++ lib/views/webview/fiat.dart
@@ -1,6 +1,6 @@
-import 'package:bicrypto/services/api_service.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:bicrypto/services/api_service.dart';
 
 class WebViewPage extends StatefulWidget {
   @override
@@ -9,67 +9,81 @@ class WebViewPage extends StatefulWidget {
 
 class _WebViewPageState extends State<WebViewPage> {
   final apiService = ApiService();
-  late CookieManager cookieManager;
-  bool isWebViewReady = false;
+  InAppWebViewController? _webViewController;
 
   @override
   void initState() {
     super.initState();
-    cookieManager = CookieManager();
-    _setCookies();
+    // Load the tokens initially.
+    apiService.loadTokens();
   }
 
-  Future<void> _setCookies() async {
-    await apiService.loadTokens();
+  Future<void> _setCookies(InAppWebViewController controller) async {
+    await apiService.loadTokens(); // Ensure the latest tokens are loaded.
 
-    for (var key in apiService.tokens.keys) {
-      String? value = apiService.tokens[key];
-      if (value != null) {
-        // Remove 'Bearer ' prefix if present
-        if (value.startsWith('Bearer ')) {
-          value = value.substring('Bearer '.length);
-        }
-        print("Setting cookie: $key=$value");
-        await cookieManager.setCookie(
-          WebViewCookie(
-            name: key,
-            value: value,
-            domain: 'v3.mash3div.com', // Verify this with your backend
-            path: '/', // Verify this with your backend
-            // Consider adding other attributes like 'expires', 'secure', etc.
-          ),
-        );
+    CookieManager cookieManager = CookieManager.instance();
+
+    for (var entry in apiService.tokens.entries) {
+      var key = entry.key;
+      var value = entry.value;
+
+      if (value != null && value.isNotEmpty) {
+        // Set each cookie using CookieManager
+        await cookieManager
+            .setCookie(
+          url: Uri.parse('https://v3.mash3div.com'),
+          name: key,
+          value: value,
+          domain: 'v3.mash3div.com',
+          path: '/',
+          isHttpOnly: false, // Set as needed
+          isSecure: true, // Set as needed
+          sameSite: HTTPCookieSameSitePolicy.LAX, // Set as needed
+          // expiresDate: ... // If you need to set an expiration date
+        )
+            .then((result) {
+          print('Cookie set: $key=$value');
+        }).catchError((error) {
+          print('Error setting cookie $key: $error');
+        });
       }
     }
 
-    // Delay WebView loading to ensure cookies are set
-    await Future.delayed(Duration(seconds: 2));
-    setState(() {
-      isWebViewReady = true;
-    });
+    // After setting cookies, load the initial URL.
+    controller.loadUrl(
+        urlRequest: URLRequest(
+      url: Uri.parse('https://v3.mash3div.com/user/wallets/fiat'),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('WebView with Cookies'),
+      appBar: AppBar(title: Text('WebView with Cookies')),
+      body: InAppWebView(
+        initialOptions: InAppWebViewGroupOptions(
+          crossPlatform: InAppWebViewOptions(
+            javaScriptEnabled: true,
+          ),
+        ),
+        onWebViewCreated: (InAppWebViewController controller) {
+          _webViewController = controller;
+          _setCookies(controller); // Set cookies when web view is created.
+        },
+        onLoadStart: (controller, url) {
+          print("WebView started loading: $url");
+        },
+        onLoadStop: (controller, url) {
+          print("WebView finished loading: $url");
+        },
+        onLoadError: (controller, url, code, message) {
+          print(
+              "WebView error: URL: $url, Error Code: $code, Error Message: $message");
+        },
+        onConsoleMessage: (controller, consoleMessage) {
+          print("Console message: ${consoleMessage.message}");
+        },
       ),
-      body: isWebViewReady
-          ? WebView(
-              initialUrl: 'https://v3.mash3div.com/user/wallets/fiat',
-              javascriptMode: JavascriptMode.unrestricted,
-              onPageStarted: (String url) {
-                print("WebView started loading: $url");
-              },
-              onPageFinished: (String url) {
-                print("WebView finished loading: $url");
-              },
-              onWebResourceError: (error) {
-                print("WebView error: ${error.description}");
-              },
-            )
-          : Center(child: CircularProgressIndicator()),
     );
   }
 }
